c6507804df822d9bdd4dee2079e8bfb6618e8736
Alexander Schätzl <tirapat@web.de>
2024-02-06 09:47:49 +0100

    Add project files.

---
+++ MSFS Flight Assistant/MSFS Flight Assistant.cpp
@@ -0,0 +1,10 @@
+// MSFS Flight Assistant.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "serverConnection.h"
+
+int main()
+{
+    connectToServer();
+}
---
+++ MSFS Flight Assistant/serverConnection.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void connectToServer() {
+	string ipAddress = "127.0.0.1";			// IP Address of the server
+	int port = 54000;						// Listening port # on the server
+
+	// Initialize WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0)
+	{
+		cerr << "Can't start Winsock, Err #" << wsResult << endl;
+		return;
+	}
+
+	// Create socket
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
+		WSACleanup();
+		return;
+	}
+
+	// Fill in a hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	// Connect to server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		cerr << "Can't connect to server, Err #" << WSAGetLastError() << endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+	std::cout << "Connected to Server!" << std::endl;
+}
+
+void sendToServer() {
+
+}
