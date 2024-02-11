3231ee0e9b7f4d5f392eba334cd5df7d3d63996a
Alexander Schätzl <tirapat@web.de>
2024-02-11 13:04:57 +0100

    Added multithreading

--- MSFS Support Server/main.cpp
+++ MSFS Support Server/main.cpp
@@ -7,90 +7,33 @@
 
 using namespace std;
 
-void main()
-{
-	// Initialze winsock
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
 
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0)
-	{
-		cerr << "Can't Initialize winsock! Quitting" << endl;
-		return;
-	}
+simData currentSimData;
 
-	// Create a socket
-	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
-	if (listening == INVALID_SOCKET)
-	{
-		cerr << "Can't create a socket! Quitting" << endl;
-		return;
-	}
+DWORD WINAPI handleEvents(LPVOID lpParam) {
+	cout << "Thread started" << endl;
 
-	// Bind the ip address and port to a socket
-	sockaddr_in hint;
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(54000);
-	hint.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+	SOCKET currentClient = (SOCKET)lpParam;
 
-	bind(listening, (sockaddr*)&hint, sizeof(hint));
-
-	// Tell Winsock the socket is for listening 
-	listen(listening, SOMAXCONN);
-	std::cout << "listening to port" << std::endl;
-
-	// Wait for a connection
-	sockaddr_in client;
-	int clientSize = sizeof(client);
-
-	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
-
-	char host[NI_MAXHOST];		// Client's remote name
-	char service[NI_MAXSERV];	// Service (i.e. port) the client is connect on
-
-	ZeroMemory(host, NI_MAXHOST); // same as memset(host, 0, NI_MAXHOST);
-	ZeroMemory(service, NI_MAXSERV);
-
-	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
-	{
-		cout << host << " connected on port " << service << endl;
-	}
-	else
-	{
-		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-		cout << host << " connected on port " <<
-			ntohs(client.sin_port) << endl;
-	}
-
-	// Close listening socket
-	closesocket(listening);
-
-	// While loop: accept and echo message back to client
 	char buf[4096];
 
-	simData currentSimData;
+	char sendData[4096];
 
-	while (true)
-	{
-		ZeroMemory(buf, 4096);
-
-		// Wait for client to send data
-		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+	while (true) {
+		int bytesReceived = recv(currentClient, buf, 4096, 0);
 		if (bytesReceived == SOCKET_ERROR)
 		{
-			cerr << "Error in recv(). Quitting" << endl;
-			break;
+			cout << "Error in recv(). Quitting" << endl;
+			return 0;
 		}
 
 		if (bytesReceived == 0)
 		{
 			cout << "Client disconnected " << endl;
-			break;
+			return 0;
 		}
 		decideStruct* determineStruct = (decideStruct*)&buf;
 
-		//cout << string(buf, 0, bytesReceived) << endl;
 		cout << char(determineStruct->flag) << endl;
 
 		if (determineStruct->flag == 0) {
@@ -99,16 +42,72 @@ void main()
 		}
 
 		else if (determineStruct->flag == 1) {
-			send(clientSocket, (const char*)&currentSimData, sizeof(currentSimData), 0);
+			send(currentClient, (const char*)&currentSimData, sizeof(currentSimData), 0);
 		}
+	}
+}
 
+
+int main()
+{
+	//master Socket
+	SOCKET sock;
+
+	//for multi Threading
+	DWORD thread;
+
+	// Initialze winsock
+	WSADATA wsaData;
+	sockaddr_in server;
+
+	// start winsock
+	int ret = WSAStartup(0x101, &wsaData);
+	if (ret != 0) {
+		return 0;
+	}
+
+	// Create a socket
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		return 0;
+	}
+
+	// Bind the ip address and port to a socket
+	server.sin_family = AF_INET;
+	server.sin_port = htons(54000);
+	server.sin_addr.S_un.S_addr = INADDR_ANY; // Could also use inet_pton .... 
+
+	if (bind(sock, (sockaddr*)&server, sizeof(server)) != 0) {
+		return 0;
+	}
+
+	// Tell Winsock the socket is for listening 
+	if (listen(sock, 5) != 0) {
+		return 0;
+	}
+	cout << "listening to port" << endl;
+
+	// Wait for a connection
+	SOCKET clientSocket;
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+
+	// While loop: accept and echo message back to client
+
+	while (true) {
+		clientSocket = accept(sock, (struct sockaddr*)&client, &clientSize);
+		cout << "Client connected!" << endl;
+
+		CreateThread(NULL, 0, handleEvents, (LPVOID)clientSocket, 0, &thread);
 	}
 
 	// Close the socket
-	closesocket(clientSocket);
+	closesocket(sock);
 
 	// Cleanup winsock
 	WSACleanup();
 
-	system("pause");
+	return 0;
 }
